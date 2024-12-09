9f0e0aaed072fdb662c3e9bb5d450d0b25bb32dd
sluismiguel <sluismiguelnunes@gmail.com>
2024-12-09 12:24:39 -0300

    Update academia.sql

--- database/academia.sql
+++ database/academia.sql
@@ -1,10 +1,15 @@
 CREATE SCHEMA academia;
 
-CREATE TABLE cliente (
+USE academia;
+
+CREATE TABLE clientes (
    id INT NOT NULL,
    nome VARCHAR (20) NOT NULL,
    telefone INT (11),
    email VARCHAR (25),
    idade INT (2),
-   observacoes VARCHAR (50)
-);
+   observacoes VARCHAR (50),
+   PRIMARY KEY (id)
+   );
+   
+ALTER TABLE clientes MODIFY id int NOT NULL AUTO_INCREMENT;
